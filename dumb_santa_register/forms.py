60296c4041707b20145a9f1686270b3c60d56648
badbread42 <badbread42@gmail.com>
2018-12-10 19:43:38 +0300

    sintax and extends

--- dumb_santa_register/forms.py
+++ dumb_santa_register/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from .models import dumb_santa
 
 class dumb_santa_form(forms.ModelForm):
+	address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'iwilldonatyou@rencap.com'}))
+	extends = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'алергии, неприязнь, нетерпимости'}))
 
 	class Meta:
-        model = dumb_santa
-        fields = ('address', 'extends',)
+		model = dumb_santa
+		fields = ('address', 'extends',)
--- dumb_santa_register/views.py
+++ dumb_santa_register/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import dumb_santa_form
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
+#def dumb_santa_register(request):
+#	form = dumb_santa_form()
+#    return render(request, 'dumb_santa_register/dumb_main.html', {})
+	
+@csrf_exempt
 def dumb_santa_register(request):
-    return render(request, 'dumb_santa_register/dumb_main.html', {})
+    if request.method == "POST":
+        form = dumb_santa_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dumb_santa_register')
+    else:
+        form = dumb_santa_form()
+    return render(request, 'dumb_santa_register/dumb_main.html', {'form': form})
