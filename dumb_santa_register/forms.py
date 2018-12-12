b397c4cf87632f015758befcd5bf7c2340dc5a56
badbread42 <badbread42@gmail.com>
2018-12-12 18:00:26 +0300

    not req

--- dumb_santa_register/forms.py
+++ dumb_santa_register/forms.py
@@ -3,7 +3,7 @@ from .models import dumb_santa
 
 class dumb_santa_form(forms.ModelForm):
 	address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'iwilldonatyou@rencap.com'}))
-	extends = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'алергии, неприязнь, нетерпимости'}))
+	extends = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'алергии, неприязнь, нетерпимости'}), required=False)
 
 	class Meta:
 		model = dumb_santa
