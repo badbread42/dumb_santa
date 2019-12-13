8e9665e04854ed98264412160c303749525f786c
Kochnev <DKochnev@rencap.com>
2019-12-13 14:36:07 +0300

    2020

--- dumb_santa_register/forms.py
+++ dumb_santa_register/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import dumb_santa
 
 class dumb_santa_form(forms.ModelForm):
-	address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'iwilldonatyou@rencap.com'}))
-	extends = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'алергии, неприязнь, нетерпимости'}), required=False)
+	address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'denchik@rencap.com'}), required=True)
+	extends = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'увлечения, хобби, тайные желания'}), required=True)
 
 	class Meta:
 		model = dumb_santa
